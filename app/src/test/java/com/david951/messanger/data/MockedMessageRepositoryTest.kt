c404ad834ed653859c2f524b3b692f145d1edcab
Devit951 <Devit951@gmail.com>
2018-08-08 14:19:04 +0500

    --amend

--- app/src/test/java/com/david951/messanger/data/MockedMessageRepositoryTest.kt
+++ app/src/test/java/com/david951/messanger/data/MockedMessageRepositoryTest.kt
@@ -1,5 +1,30 @@
 package com.david951.messanger.data
 
-import org.junit.Assert.*
+import com.david951.messanger.presentation.MessageRepository
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class MockedMessageRepositoryTest
+@RunWith(JUnit4::class)
+class MockedMessageRepositoryTest{
+
+    private val testPage = 2
+    private val mockedRepository: MessageRepository = MockedMessageRepository()
+
+    @Test
+    fun shouldPageCountEquals(){
+        assert(testPage == mockedRepository.pagesCount())
+    }
+
+    @Test
+    fun shouldMessageListSizeEqualsOnFirstPage(){
+        val messageListSize = 7
+        assert(messageListSize == mockedRepository.getAllMessages(1).size)
+    }
+
+    @Test
+    fun shouldMessageListSizeEqualsOnSecondPage(){
+        val messageListSize = 5
+        assert(messageListSize == mockedRepository.getAllMessages(2).size)
+    }
+}
